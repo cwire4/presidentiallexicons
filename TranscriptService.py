320243583c9583cca442161e691e0da4e39ee9b5
Steven <scjackson@linux.com>
2017-04-04 16:17:42 -0400

    fixed

--- TranscriptService.py
+++ TranscriptService.py
@@ -1,4 +1,10 @@
-import Transcript
+from Transcript import Transcript
+from collections import defaultdict
+
+import glob
+import os
+
+paths = ""
 
 
 def sentence_to_wordlist(raw):
@@ -7,12 +13,12 @@ def sentence_to_wordlist(raw):
     return cleaned_words
 
 
-def sorted_paths(individual_path):
-    return sorted(glob.glob(transcript_paths))
+def sorted_paths(paths):
+    return sorted(glob.glob(paths))
 
 
-def transcript_dirs_list():
-    return os.walk("transcripts")
+def transcript_dirs_list(transcript_dir):
+    return os.walk(transcript_dir)
 
 
 def get_transcripts_list(paths):
@@ -25,4 +31,39 @@ def get_transcripts_list(paths):
 
 def valid_sentence(sentence):
     return len(sentence) > 0
+
+
+def inspect_sentences(transcript):
+    # Let's look at each sentence
+    for sentence in transcript.sentences:
+
+        # Sentences gotta sentence
+        if valid_sentence(sentence):
+            inspect_words(sentence, transcript)
+
+
+def inspect_words(wordlist, transcript):
+    # Split up the words
+    words = sentence_to_wordlist(wordlist)
+
+    # Gettin Loopy With Loops(tm), Let's evaluate each word.
+    # @todo is there a method to do this rather than looping?
+    for word in words:
+        # @todo need to strip words of punctuation - counts are off
+        # Increment individual word count
+        transcript.word_frequencies[word] += 1
+
+        # Get sentence length, add that to total count
+        transcript.word_count += len(words)
+
+        # Yo dawg I heard you like lengths. I'm gonna length your length.
+        transcript.word_lengths[len(word)] += 1
+
+
+def word_frequencies(transcript_list):
+    word_freqs = defaultdict(int)
+    for transcript in transcript_list:
+        for word, count in transcript.word_frequencies.items():
+            word_freqs[word] += count
+    return word_freqs
 
